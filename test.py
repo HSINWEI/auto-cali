297dd8ec602cb95b4d8b1908841516ee74f287e5
hwchen <chen.hsinwei@gmail.com>
2015-09-27 23:28:48 +0800

    clean project
    add launch files

--- test.py
+++
@@ -1,8 +0,0 @@
-from processSingleXml import processSingleXml
-import specs
-import sys
-if __name__ == '__main__':
-    for filename in sys.argv[1:]:
-        processSingleXml(filename)
-        specs_obj = specs.SPECS(filename)
-        
