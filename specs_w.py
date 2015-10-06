262385a96b752852451421b1034a21b07c50bfb7
Administrator <Administrator@172.18.66.159>
2015-10-06 10:52:49 +0800

    add xml header change writeCalibratedXml() add getPeakLocation()

--- specs_w.py
+++ specs_w.py
@@ -9,6 +9,49 @@ import os
 import xml.etree.cElementTree as ET
 
 VERBOSE = 0
+xmlheader = '''<?xml version="1.0"?>
+<!-- CORBA XML document created by XMLSerializer2 1.6 at 2015-07-21 16:24:09 UTC, from SL 2.78-r28574 built 2012-08-10 09:11:32 UTC -->
+<!DOCTYPE any [
+<!ELEMENT any (any|typecode|sequence|array|struct|union|exception|string|objectref|enum|boolean|char|octet|short|ushort|long|ulong|float|double)?>
+<!ATTLIST any name NMTOKEN #IMPLIED type_id CDATA #IMPLIED type_name NMTOKEN #IMPLIED label NMTOKEN #IMPLIED version CDATA #IMPLIED>
+<!ELEMENT typecode (any|typecode|sequence|array|struct|union|exception|string|objectref|enum|boolean|char|octet|short|ushort|long|ulong|float|double)?>
+<!ATTLIST typecode name NMTOKEN #IMPLIED type_id CDATA #IMPLIED type_name NMTOKEN #IMPLIED label NMTOKEN #IMPLIED>
+<!ELEMENT sequence ((any|typecode|sequence|array|struct|union|exception|string|objectref)+|(enum|boolean|char|octet|short|ushort|long|ulong|float|double))?>
+<!ATTLIST sequence name NMTOKEN #IMPLIED type_id CDATA #IMPLIED type_name NMTOKEN #IMPLIED length NMTOKEN #REQUIRED bound NMTOKEN #IMPLIED label NMTOKEN #IMPLIED>
+<!ELEMENT array ((any|typecode|sequence|array|struct|union|exception|string|objectref)+|(enum|boolean|char|octet|short|ushort|long|ulong|float|double))?>
+<!ATTLIST array name NMTOKEN #IMPLIED type_id CDATA #IMPLIED type_name NMTOKEN #IMPLIED length NMTOKEN #REQUIRED label NMTOKEN #IMPLIED>
+<!ELEMENT struct (any|typecode|sequence|array|struct|union|exception|string|objectref|enum|boolean|char|octet|short|ushort|long|ulong|float|double)*>
+<!ATTLIST struct name NMTOKEN #IMPLIED type_id CDATA #REQUIRED type_name NMTOKEN #IMPLIED label NMTOKEN #IMPLIED>
+<!ELEMENT union (any|typecode|sequence|array|struct|union|exception|string|objectref|enum|boolean|char|octet|short|ushort|long|ulong|float|double)*>
+<!ATTLIST union name NMTOKEN #IMPLIED type_id CDATA #REQUIRED type_name NMTOKEN #IMPLIED label NMTOKEN #IMPLIED>
+<!ELEMENT exception (any|typecode|sequence|array|struct|union|exception|string|objectref|enum|boolean|char|octet|short|ushort|long|ulong|float|double)*>
+<!ATTLIST exception name NMTOKEN #IMPLIED type_id CDATA #REQUIRED type_name NMTOKEN #IMPLIED label NMTOKEN #IMPLIED>
+<!ELEMENT string (#PCDATA)>
+<!ATTLIST string name NMTOKEN #IMPLIED type_id CDATA #IMPLIED type_name NMTOKEN #IMPLIED bound NMTOKEN #IMPLIED label NMTOKEN #IMPLIED>
+<!ELEMENT objectref (#PCDATA)>
+<!ATTLIST objectref name NMTOKEN #IMPLIED type_id CDATA #REQUIRED type_name NMTOKEN #IMPLIED label NMTOKEN #IMPLIED>
+<!ELEMENT enum (#PCDATA)>
+<!ATTLIST enum name NMTOKEN #IMPLIED type_id CDATA #REQUIRED type_name NMTOKEN #IMPLIED values NMTOKENS #REQUIRED label NMTOKEN #IMPLIED>
+<!ELEMENT boolean (#PCDATA)>
+<!ATTLIST boolean name NMTOKEN #IMPLIED type_id CDATA #IMPLIED type_name NMTOKEN #IMPLIED label NMTOKEN #IMPLIED>
+<!ELEMENT char (#PCDATA)>
+<!ATTLIST char name NMTOKEN #IMPLIED type_id CDATA #IMPLIED type_name NMTOKEN #IMPLIED label NMTOKEN #IMPLIED>
+<!ELEMENT octet (#PCDATA)>
+<!ATTLIST octet name NMTOKEN #IMPLIED type_id CDATA #IMPLIED type_name NMTOKEN #IMPLIED label NMTOKEN #IMPLIED>
+<!ELEMENT short (#PCDATA)>
+<!ATTLIST short name NMTOKEN #IMPLIED type_id CDATA #IMPLIED type_name NMTOKEN #IMPLIED label NMTOKEN #IMPLIED>
+<!ELEMENT ushort (#PCDATA)>
+<!ATTLIST ushort name NMTOKEN #IMPLIED type_id CDATA #IMPLIED type_name NMTOKEN #IMPLIED label NMTOKEN #IMPLIED>
+<!ELEMENT long (#PCDATA)>
+<!ATTLIST long name NMTOKEN #IMPLIED type_id CDATA #IMPLIED type_name NMTOKEN #IMPLIED label NMTOKEN #IMPLIED>
+<!ELEMENT ulong (#PCDATA)>
+<!ATTLIST ulong name NMTOKEN #IMPLIED type_id CDATA #IMPLIED type_name NMTOKEN #IMPLIED label NMTOKEN #IMPLIED>
+<!ELEMENT float (#PCDATA)>
+<!ATTLIST float name NMTOKEN #IMPLIED type_id CDATA #IMPLIED type_name NMTOKEN #IMPLIED label NMTOKEN #IMPLIED>
+<!ELEMENT double (#PCDATA)>
+<!ATTLIST double name NMTOKEN #IMPLIED type_id CDATA #IMPLIED type_name NMTOKEN #IMPLIED label NMTOKEN #IMPLIED>
+]>
+'''
 
 class SPECS_W(SPECS):
     """ Represent a SPECSLab .xml output as a python object. Construct with:
@@ -60,10 +103,20 @@ class SPECS_W(SPECS):
             if group.get('type_name') == "RegionGroup":
                 self.groups.append(SPECSGroup_W(group))
     
-    def writeCalibratedXml(self):
-        mainname, extname = os.path.splitext(self.filename)
-        newfilename = mainname + '-calibrated.xml'
-        self.tree.write(newfilename)
+    def writeCalibratedXml(self, working_dir=None):
+        global xmlheader
+        if working_dir == None:
+            working_dir = os.getcwd()
+        print 'working dir ' + working_dir
+        filepath, filename = os.path.split(self.filename) 
+        mainname, extname = os.path.splitext(filename)
+        newfilename = os.path.join( working_dir, mainname + '-calibrated.xml')    
+        noheaderilename = os.path.join( working_dir, mainname + '-noheader.xml')
+        self.tree.write( noheaderilename)
+        with file(noheaderilename, 'r') as original: data = original.read()
+        with file(newfilename, 'w') as modified: modified.write(xmlheader + data)
+        original.close()
+        modified.close()
         return newfilename
 
 class SPECSGroup_W(SPECSGroup):
@@ -109,3 +162,10 @@ class SPECSRegion_W(SPECSRegion):
 
     def setXmlRegionDataName(self, name):
         self.xmlregion[0].text = name
+
+    def calculate_binding_axis(self,excitation_energy):
+        self.binding_axis = excitation_energy - self.kinetic_axis
+    
+    def getPeakLocation(self):
+        return float(self.xmlregion.find(".//struct[@name='x']//double[@name='value']").text)   
+
